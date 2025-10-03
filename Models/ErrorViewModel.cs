327065ee16e735f8d1191c13a2a44f5f6bbfa1aa
Elishua Emmanuel Naidoo <166371667+Elishua007@users.noreply.github.com>
2025-10-03 09:20:31 +0200

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ICE_TASK_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace ICE_TASK_2.Models
+{
+    public class Product : ITableEntity
+    {
+        public string PartitionKey { get; set; } = "Products";
+        public string RowKey { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; } = default!;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public bool Clearance { get; set; }
+        public ETag ETag { get; set; } = ETag.All;
+        public DateTimeOffset? Timestamp { get; set; }
+    }
+}
