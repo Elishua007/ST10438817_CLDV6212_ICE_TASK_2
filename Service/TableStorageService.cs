98a203be727a5b35d2de99314049604225bba386
Elishua Emmanuel Naidoo <166371667+Elishua007@users.noreply.github.com>
2025-10-03 09:25:17 +0200

    Add files via upload

---
+++ Service/TableStorageService.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Data.Tables;
+using ICE_TASK_2.Models;
+
+namespace ICE_TASK_2.Service
+{
+    public class TableStorageService
+    {
+        private readonly TableClient _tableClient;
+
+        public TableStorageService(IConfiguration config)
+        {
+            string conn = config["AzureTableStorage:ConnectionString"];
+            string tableName = config["AzureTableStorage:TableName"] ?? "Products";
+
+            var service = new TableServiceClient(conn);
+            _tableClient = service.GetTableClient(tableName);
+            _tableClient.CreateIfNotExists();
+        }
+
+        public async Task AddProductAsync(Product product) =>
+            await _tableClient.UpsertEntityAsync(product, TableUpdateMode.Replace);
+
+        public async IAsyncEnumerable<Product> QueryProductsAsync(string partitionKey)
+        {
+            var q = _tableClient.QueryAsync<Product>(p => p.PartitionKey == partitionKey);
+            await foreach (var item in q)
+                yield return item;
+        }
+    }
+}
